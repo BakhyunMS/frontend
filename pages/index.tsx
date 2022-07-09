f5f0bdb4e143ba8290374f67fa32e4dc71a33766
chul0721 <chul070721@naver.com>
2022-07-09 22:37:45 +0900

    feat: main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,30 @@
 import type { NextPage } from 'next'
+import Image from 'next/image'
 
 const Main: NextPage = () => {
-  return <></>
+  return (
+    <div className="w-full h-screen bg-hero bg-cover">
+      <div className="w-full h-full flex flex-col justify-center">
+        <div className="w-2/5 h-3/5 grid self-center items-center justify-items-center text-2xl space-y-2 bg-white rounded-3xl p-2 shadow-2xl">
+          <div className="grid">
+            <div>
+              <Image src="/static/images/logo.webp" width={80} height={80} />
+            </div>
+            <p>환영합니다.</p>
+            <p className="font-pretandard-semibold">로그인하여 서비스를 이용하세요.</p>
+            <button className="flex justify-self-center border border-slate-100 hover:opacity-60 shadow-xl p-4 px-10 text-lg font-pretandard-semibold rounded-xl items-center space-x-2 mt-4">
+              <Image src="/static/images/google.webp" width={20} height={20} />
+              <p>Google 학교 계정으로 로그인</p>
+            </button>
+            <p className="text-base font-pretandard-light pt-3">
+              * 학교에서 지급 받으신 구글 계정을 통해 로그인하세요. <br />
+              &nbsp;&nbsp; 자세한 사항은 담당 선생님께 문의하세요.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Main
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        hero: "url('/static/images/hero.webp')"
+      },
+      fontFamily: {
+        'pretandard-regular': 'Pretendard-Regular',
+        'pretandard-bold': 'Pretendard-Bold',
+        'pretandard-extrabold': 'Pretendard-ExtraBold',
+        'pretandard-light': 'Pretendard-Light',
+        'pretandard-medium': 'Pretendard-Medium',
+        'pretandard-semibold': 'Pretendard-SemiBold',
+        'pretandard-thin': 'Pretendard-Thin',
+        'pretandard-extralight': 'Pretendard-ExtraLight'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
